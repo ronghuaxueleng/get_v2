794aa51778587d4d6d969f2970409d56e808818a
caoqiang <caoqiang>
2023-10-23 09:16:45 +0800

    up

--- chichi/get_root_urls.py
+++ chichi/get_root_urls.py
@@ -1,12 +1,15 @@
 import json
-import re
+import os
+import urllib
+from multiprocessing import cpu_count, Pool
 
 import requests
 from bs4 import BeautifulSoup
 
 from chichi.info import Info, Image
 
-root_url = "https://www.chichi-pui.com/api/posts/following/?age_limit=ALL"
+domain = "https://www.chichi-pui.com"
+root_url = f"{domain}/api/posts/following/?age_limit=ALL"
 
 payload = {}
 headers = {
@@ -52,18 +55,75 @@ def get_url(url):
         print(e)
 
 
+def save_image_url():
+    posts = Info.select().where(Info.grabState != 1).execute()
+    if len(posts) > 0:
+        for post in posts:
+            try:
+                url = f"{domain}{post.url}"
+                response = requests.request("GET", url, headers=headers, data=payload)
+                text = response.text
+                soup = BeautifulSoup(text, "lxml")
+                image_link_container = soup.select('.js-main-image-link')
+                print(f"页面【{url}】")
+                print(f"获取{len(image_link_container)}张图片")
+                for image_link in image_link_container:
+                    try:
+                        image_data = {}
+                        image_data['root_url'] = url
+                        image_data['image_url'] = image_link.get("href")
+                        Image.insert(image_data).execute()
+                    except Exception as e:
+                        print(e)
+                Info.update(grabState=1).where(Info.postId == post.postId).execute()
+            except Exception as e:
+                print(e)
+
+
+def request_image(url, image_name, PROXIES=None):
+    print(("正在下载 %s" % image_name))
+    try:
+        os.makedirs("images", exist_ok=True)
+        req = urllib.request.Request(url)
+        req.add_header('User-Agent', headers['user-agent'])
+        req.add_header('Cache-Control', 'no-cache')
+        req.add_header('Accept', '*/*')
+        req.add_header('Accept-Encoding', 'gzip, deflate')
+        req.add_header('Connection', 'Keep-Alive')
+
+        if PROXIES is not None:
+            opener = urllib.request.build_opener(urllib.request.ProxyHandler(PROXIES))
+            urllib.request.install_opener(opener)
+
+        resp = urllib.request.urlopen(req)
+
+        respHtml = resp.read()
+        path = 'images/%s' % image_name
+
+        binfile = open(path, "wb")
+        binfile.write(respHtml)
+
+        binfile.close()
+        Image.update(grabState=1).where(Image.image_url == url).execute()
+        print(("%s 下载成功" % image_name))
+        return True
+    except Exception as e:
+        print(e)
+        print(("%s 下载失败" % image_name))
+        return False
+
+
+def download_image(proxy=None):
+    images = Image.select().where(Image.grabState != 1).execute()
+    if len(images) > 0:
+        cpu_counts = cpu_count()
+        print("cup数量：{}".format(cpu_counts))
+        pool = Pool(processes=cpu_counts)
+        for img in images:
+            request_image(img.image_url, img.id + ".png", PROXIES=proxy)
+        pool.close()
+        pool.join()  # 运行完所有子进程才能顺序运行后续程序
+
+
 if __name__ == '__main__':
-    url = "https://www.chichi-pui.com/posts/d77f138d-0902-409d-9061-d8be06b331c5/"
-    response = requests.request("GET", url, headers=headers, data=payload)
-    text = response.text
-    soup = BeautifulSoup(text, "lxml")
-    image_link_container = soup.select('.js-main-image-link')
-    print(len(image_link_container))
-    for image_link in image_link_container:
-        try:
-            image_data = {}
-            image_data['root_url'] = image_link.get("url")
-            image_data['image_url'] = image_link.get("href")
-            Image.insert(image_data).execute()
-        except Exception as e:
-            print(e)
+    save_image_url()
